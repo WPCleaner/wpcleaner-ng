e20a1427b1ec2c719b62aff30f8041c0607b11a3
Nicolas VERVELLE <nicolas.vervelle@quicksign.com>
2025-08-09 20:15:32 +0200

    Extract RestClient creation

---
+++ wpcleaner-api/src/main/java/org/wpcleaner/api/api/ApiRestClient.java
@@ -0,0 +1,40 @@
+package org.wpcleaner.api.api;
+
+/*
+ * SPDX-FileCopyrightText: © 2025 Nicolas Vervelle <[WPCleaner](https://github.com/WPCleaner)>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import org.wpcleaner.api.wiki.definition.WikiDefinition;
+
+@Service
+public class ApiRestClient {
+
+  private final RestClient.Builder clientBuilder;
+  private final Map<WikiDefinition, RestClient> restClients;
+
+  public ApiRestClient() {
+    this.clientBuilder = RestClient.builder();
+    this.restClients = new ConcurrentHashMap<>();
+  }
+
+  public RestClient getRestClient(final WikiDefinition wiki) {
+    return restClients.computeIfAbsent(wiki, this::createRestClient);
+  }
+
+  private RestClient createRestClient(final WikiDefinition wiki) {
+    return clientBuilder
+        .baseUrl(wiki.apiUrl())
+        .defaultHeader(HttpHeaders.ACCEPT_CHARSET, "utf-8")
+        .defaultHeader(HttpHeaders.ACCEPT_ENCODING, "gzip")
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+        .defaultHeader(HttpHeaders.USER_AGENT, "WPCleaner")
+        // TODO: https://foundation.wikimedia.org/wiki/Policy:Wikimedia_Foundation_User-Agent_Policy
+        .build();
+  }
+}
--- wpcleaner-api/src/main/java/org/wpcleaner/api/api/query/meta/tokens/ApiTokens.java
+++ wpcleaner-api/src/main/java/org/wpcleaner/api/api/query/meta/tokens/ApiTokens.java
@@ -10,15 +10,20 @@ import jakarta.annotation.Nullable;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 import org.wpcleaner.api.api.ApiParameters;
+import org.wpcleaner.api.api.ApiRestClient;
 import org.wpcleaner.api.api.query.QueryParameters;
 import org.wpcleaner.api.wiki.definition.WikiDefinition;
 
 @Service
 public class ApiTokens {
+
+  private final ApiRestClient restClient;
+
+  public ApiTokens(final ApiRestClient restClient) {
+    this.restClient = restClient;
+  }
 
   public Tokens requestTokens(
       final WikiDefinition wiki, final Collection<TokensParameters.Type> tokens) {
@@ -31,15 +36,12 @@ public class ApiTokens {
   @Nullable
   private Response internalRequestTokens(
       final WikiDefinition wiki, final Collection<TokensParameters.Type> tokens) {
-    final RestClient restClient = RestClient.builder().build();
     return restClient
+        .getRestClient(wiki)
         .get()
         .uri(
             uriBuilder ->
                 uriBuilder
-                    .scheme("https")
-                    .host(wiki.mainHost())
-                    .path(wiki.apiPath())
                     .queryParam(ApiParameters.ACTION.value, ApiParameters.Action.QUERY.value)
                     .queryParam(ApiParameters.FORMAT.value, ApiParameters.Format.JSON.value)
                     .queryParam(QueryParameters.META.value, QueryParameters.Meta.TOKENS.value)
@@ -47,15 +49,10 @@ public class ApiTokens {
                         TokensParameters.TYPE.value,
                         tokens.stream().map(type -> type.value).collect(Collectors.joining("|")))
                     .build())
-        .header(HttpHeaders.ACCEPT_CHARSET, "utf-8")
-        .header(HttpHeaders.ACCEPT_ENCODING, "gzip")
-        .header(HttpHeaders.CONTENT_TYPE, "application/json")
-        .header(HttpHeaders.USER_AGENT, "WPCleaner")
         .retrieve()
         .body(Response.class);
   }
 
-  @SuppressWarnings("unused")
   private record Response(
       @JsonProperty("batchcomplete") boolean batchComplete,
       @JsonProperty("query") ResponseQuery query) {}
